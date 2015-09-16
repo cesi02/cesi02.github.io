569ee513e5408e5011aa8fca4a8b7178905fa50e
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-09-17 01:15:38 +0200

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,6 +12,7 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var domain = "4";
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -24,7 +25,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    domain: domain
                 },
                 cache: false,
                 success: function() {
